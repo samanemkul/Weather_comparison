ff7fcd36875f686e55c7ca585366b3e6696d36ad
samanemkul <nemkulsama1@gmail.com>
2024-08-23 17:29:32 +0545

    Added

---
+++ weather/weather_app/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+import requests
+import datetime
+
+def index(request):
+    api_key='<own API key'
+    current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+    forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
+
+    if request.method == 'POST':
+        city1 = request.POST['city1']
+        city2 = request.POST.get('city2', None)
+
+        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, api_key, current_weather_url, forecast_url)
+
+        if city2:
+            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, api_key, current_weather_url,
+                                                                         forecast_url)
+        else:
+            weather_data2, daily_forecasts2 = None, None
+
+        context = {
+            'weather_data1': weather_data1,
+            'daily_forecasts1': daily_forecasts1,
+            'weather_data2': weather_data2,
+            'daily_forecasts2': daily_forecasts2,
+        }
+
+        return render(request, 'home.html', context)
+    else:
+        return render(request, 'home.html')
+
+
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    
+    # Check if the response contains 'coord'
+    if 'coord' in response:
+        lat, lon = response['coord']['lat'], response['coord']['lon']
+        forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+    else:
+        # Handle the case where 'coord' is not in the response
+        return None, []
+
+    # Prepare weather data
+    weather_data = {
+        'city': city,
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+    daily_forecasts = []
+    
+    # Check if the 'daily' key exists in the forecast response
+    if 'daily' in forecast_response:
+        for daily_data in forecast_response['daily'][:5]:
+            daily_forecasts.append({
+                'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+                'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+                'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+                'description': daily_data['weather'][0]['description'],
+                'icon': daily_data['weather'][0]['icon'],
+            })
+    else:
+        # Handle the missing 'daily' data appropriately
+        daily_forecasts = None
+
+    return weather_data, daily_forecasts
+
